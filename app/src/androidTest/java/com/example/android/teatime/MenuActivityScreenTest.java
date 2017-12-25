40e1ca543a3b7c38fb36a4d4577218d075ba5c55
RafidiaAR <rafidiaar007@gmail.com>
2017-12-24 19:49:37 -0800

    Test AdapterViews with Espresso

--- app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -7,25 +7,29 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
 
 /**
  * Created by Rafid on 12/24/2017.
  */
 
 @RunWith(AndroidJUnit4.class)
-public class OrderActivityBasicClass {
-    @Rule public ActivityTestRule<OrderActivity> mActivityTestRule = new ActivityTestRule<>(OrderActivity.class);
+public class MenuActivityScreenTest {
+    public static final String TEA_NAME = "Green Tea";
+
+    @Rule
+    public ActivityTestRule<MenuActivity> mActivityActivityTestRule = new ActivityTestRule<> (MenuActivity.class);
 
     @Test
-    public void clickIncrementButton_ChangesQuantityAndCost()
+    public void clickGridViewItem_OpensOrderActivity()
     {
-        onView((withId(R.id.increment_button))).perform(click());
-        onView(withId(R.id.quantity_text_view)).check(matches(withText("1")));
-        onView(withId(R.id.cost_text_view)).check(matches(withText("$5.00")));
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(1).perform(click());
+        onView(withId(R.id.tea_name_text_view)).check(matches(withText(TEA_NAME)));
     }
 }
