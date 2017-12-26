4be9e1c184dc95332f2eb9d4ac5e322b9516fddc
RafidiaAR <rafidiaar007@gmail.com>
2017-12-25 22:54:16 -0800

    Final Commit

--- app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/IdlingResourceMenuActivityTest.java
@@ -1,5 +1,6 @@
 package com.example.android.teatime;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -9,6 +10,11 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
 
 /**
  * Created by Rafid on 12/24/2017.
@@ -25,15 +31,18 @@ public class IdlingResourceMenuActivityTest {
 
     @Before
     public void registerIdlingResource() {
-
+        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
+        Espresso.registerIdlingResources(mIdlingResource);
     }
     @Test
     public void idlingResourceTest() {
-
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(0).perform(click());
     }
 
     @After
     public void unregisterIdlingResource() {
-
+        if (mIdlingResource != null) {
+            Espresso.unregisterIdlingResources(mIdlingResource);
+        }
     }
 }
